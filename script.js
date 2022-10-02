32824e6559bc8488aa8ebe73bad60714839f5b70
kevin george <kevingeorgekuruvila@gmail.com>
2022-10-02 20:24:03 +0530

    final touches

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 let button = document.querySelector(".button");
-let result = Number(document.querySelector(".result"));
 button.addEventListener("click", function () {
+  let result = document.querySelector(".result");
   let initialPricePaid = Number(document.querySelector(".initial-price").value);
   let initialShares = Number(document.querySelector(".initial-shares").value);
   let currentPrize = Number(document.querySelector(".current-prize").value);
   let desiredaverage = Number(document.querySelector(".avg-needed").value);
-  let n;
-  console.log(
-    String(
-      (initialPricePaid * initialShares + currentPrize * n) /
-        (initialShares + n)
-    )
-  );
-
-  result.textContent = initialPricePaid;
+  let divider = (desiredaverage * 10 - currentPrize * 10) / 10;
+  let value =
+    initialPricePaid * initialShares -
+    (desiredaverage * initialShares) / divider;
+  console.log(value);
+  result.textContent = value;
 });
